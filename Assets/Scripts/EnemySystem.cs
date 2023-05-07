a9d588d5b866e9d534f7b623d44bcb4356409df0
atingderID <stellahappy650530@gmail.com>
2023-05-07 16:35:27 +0800

    Update 設定完敵人系統

---
+++ Assets/Scripts/EnemySystem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 敵人系統
+/// 1. 追蹤玩家方向
+/// 2. 翻面
+/// </summary>
+public class EnemySystem : MonoBehaviour
+{
+	[Header("追蹤速度"), Range(0, 100)]
+	public float moveSpeed = 0.01f;
+
+	public Transform player;
+
+	private void Awake()
+	{
+		player = GameObject.Find("企鵝主角").transform;
+	}
+	private void Update()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+	}
+}
--- Assets/Scripts/SpawnSystem.cs
+++ Assets/Scripts/SpawnSystem.cs
@@ -7,4 +7,16 @@ public class SpawnSystem : MonoBehaviour
 
 	[Header("怪物預製物")]
 	public GameObject prefabEnemy;
+
+	private void SpawnEnemy()
+	{
+		//生成物件的功能(生成物件，生成座標，生成角度)
+		Instantiate(prefabEnemy, transform.position, transform.rotation);
+	}
+
+	private void Awake()
+	{
+		//重複呼叫功能的方式("要重複呼叫的方法",幾秒後開始,間隔幾秒)
+		InvokeRepeating("SpawnEnemy",0,interval);
+	}
 }
