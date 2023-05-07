cdeadff574916df3338cfceece4ba70f134ca3fe
atingderID <stellahappy650530@gmail.com>
2023-05-07 12:14:34 +0800

    Update完成控制系統

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -5,6 +5,7 @@ public class ControlSystem : MonoBehaviour
 	//流程: 開一個新的程式，把程式丟到場景中的角色或空物件上
 	//宣告一個函式變數，讓物件上的程式依照函式類型提供一個空格，空格中要記得拉它需要讀取的物件過去
 	//函數會給變數一些功能(不確定是否可以如此稱呼例如 Rigidbody2D有velocity)
+	//ctrl+K+D可以快速整理縮排
 
 	[Header("移動速度")]
 	[Range(0.5f,99.9f)]
@@ -31,9 +32,27 @@ public class ControlSystem : MonoBehaviour
 
 	private void Update()
 	{
+		MoveandAnimation();
+	}
+
+	private void MoveandAnimation()
+	{
 		float h = Input.GetAxis("Horizontal");
 		//取得水平軸向的值，Unity預設水平軸向為A、D以及左右方向鍵。取得玩家案鍵請放Update事件中
 		float v = Input.GetAxis("Vertical");
-		rig.velocity = new Vector3(h, v, 0)*moveSpeed;
+		rig.velocity = new Vector3(h, v, 0) * moveSpeed;
+
+		//print(Input.GetKeyDown(KeyCode.A));
+
+		if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			transform.eulerAngles = new Vector3(0, 0, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			transform.eulerAngles = new Vector3(0, 180, 0);
+		}
+		//動畫參數ani控制主角，parRun則是控制走路動畫，h是水平移動，所以往右邊移動就會出現走路動畫
+		ani.SetBool(parRun, h != 0 || v != 0);
 	}
 }
