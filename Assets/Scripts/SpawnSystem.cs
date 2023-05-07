a0ad4c20b4996f11184fc631ffb3b6e0252a8872
atingderID <stellahappy650530@gmail.com>
2023-05-07 12:58:41 +0800

    Update 初始設定怪物生成

--- Assets/Scripts/SpawnSystem.cs
+++ Assets/Scripts/SpawnSystem.cs
@@ -1,6 +1,10 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SpawnSystem : MonoBehaviour
 {
+	[Header("生成間隔"),Range(0,10)]
+	public float interval = 3.5f;
 
+	[Header("怪物預製物")]
+	public GameObject prefabEnemy;
 }
