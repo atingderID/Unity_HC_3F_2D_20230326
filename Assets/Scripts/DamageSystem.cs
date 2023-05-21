566dc308c8113a6763a55c3d086f3ac8f9869faa
atingderID <130262585+atingderID@users.noreply.github.com>
2023-05-21 13:00:27 +0800

    Update 武器傷害系統

---
+++ Assets/Scripts/DamageSystem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DamageSystem : MonoBehaviour
+{
+	//使用HealthData中的血量資料
+	[Header("血量資料")]
+	public HealthData data;
+	//碰撞事件OCE，判斷碰撞的物體為何
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		//print(collision.gameObject);
+		if (collision.gameObject.name.Contains("武器"))
+		{
+			print("ㄚㄚㄚㄚ我被打到了");
+		}
+	}
+}
--- Assets/Scripts/HealthData.cs
+++ Assets/Scripts/HealthData.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
-
+//血量如果儲存在各個場景中的物件很難管理，需要調整就要一個一個進入物件調整，因此我們利用ScriptableObject(腳本化物件)，將所有資料儲存在專案中(類似prefab)，就可以更好的管理
+//在清單中新增一個資料夾(選項名稱/子選項)
 [CreateAssetMenu(menuName ="Stella/Data Health")]
 public class HealthData : ScriptableObject
 {
