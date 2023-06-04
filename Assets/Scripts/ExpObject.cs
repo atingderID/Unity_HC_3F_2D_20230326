4c8e92e247eb2a22674deb936fcd60f2327546cc
atingderID <130262585+atingderID@users.noreply.github.com>
2023-06-04 17:15:13 +0800

    Update 升級系統

---
+++ Assets/Scripts/ExpObject.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class ExpObject : MonoBehaviour
+{
+	[Header("可以吃這個物件的名稱")]
+	public string nameTarget = "企鵝主角";
+	[Header("移動速度"), Range(0, 500)]
+	public float moveSpeed = 3.5f;
+	[Header("吃掉物件的距離"),Range(0,5)]
+	public float eatDistance = 1.5f;
+	[Header("經驗值"),Range(0,5000)]
+	public float exp = 30f;
+
+	private Transform player;
+	private bool playerInArea=false;
+	private LevelManager levelManager;
+
+	private void Awake()
+	{
+		player = GameObject.Find(nameTarget).transform;
+		levelManager = FindObjectOfType<LevelManager>();
+	}
+
+	private void Update()
+	{
+		if (playerInArea)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+			float distance = Vector2.Distance(transform.position, player.position);
+			if (distance<eatDistance)
+			{
+				levelManager.GetExp(exp);
+				//傳送經驗值物件給等級管理器
+				Destroy(gameObject);
+			}
+		}
+	}
+	//要勾選is trigger物件才可以偵測
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		//輸入進入經驗值物件碰撞範圍內的物件名稱
+		//print($"<color=#ff6699>{collision.name}</color>")
+
+		if (collision.name.Contains(nameTarget))
+		{
+			//玩家進入範圍=勾選
+			playerInArea = true;
+		}
+	}
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,61 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+	[Header("等級與經驗值介面")]
+	//要用程式修改UI上的文字
+	public TextMeshProUGUI textLv;
+	public TextMeshProUGUI textExp;
+	public Image imgExp;
+	[Header("等級上限"),Range(0,500)]
+	public int lvMax = 100;
+
+	private int lv = 1;
+	private float exp;
+	private float expNeed = 100;
+	//每次升級需要的經驗值
+
+	public float[] expNeeds = { 100, 200, 300 };
+	//使用陣列儲存每次升級需要的經驗值
+
+	[ContextMenu("更新經驗值需求表")]
+	//在Unity中LevelManager上按右鍵就會出現，程式有改就要按一次
+	private void UpdateExpNeeds()
+	{
+		expNeeds = new float[lvMax];
+		for(int i=0;i< lvMax; i++)
+		{
+			expNeeds[i] = (i+1) * 100;
+		}
+	}
+
+	/// <summary>
+	/// 獲得經驗值
+	/// </summary>
+	/// <param name="getExp">取得經驗值浮點數</param>
+	public void GetExp(float getExp)
+	{
+		exp += getExp;
+		//exp=getExp+exp
+		print($"<color=yellow>當前經驗值:{exp}</color>");
+		
+		//如果經驗值>=當前等級需求並且等級<等級上限 就升級
+		if (exp >= expNeeds[lv - 1] && lv<lvMax)
+		{
+			exp = expNeeds[lv - 1];
+			//計算多出來的經驗
+			lv++;
+			//等級提升
+			textLv.text = $"Lv{lv}";
+			//更新等級介面
+		}
+
+		textExp.text = $"{exp}/{expNeeds[lv - 1]}";
+		imgExp.fillAmount = exp / expNeeds[lv - 1];
+
+		
+	}
+}
+
